adede29fd6e2bf1d3e5b6dccefdcf0e58befbc46
Robert Bukowiecki <rob.ecki19@gmail.com>
2021-03-01 19:53:23 +0100

    15.5

---
+++ src/components/Card/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Card.scss";
+import PropTypes from "prop-types";
+
+class Card extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+  };
+
+  static defaultProps = {
+    title: <h3>card_name</h3>,
+  };
+
+  render() {
+    const { title } = this.props;
+    return (
+      <section className={styles.component}>
+        <p className={styles.component}>{title}</p>
+      </section>
+    );
+  }
+}
+
+export default Card;
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import styles from './Column.scss';
-import Hero from '../Hero/Hero.js';
-import PropTypes from 'prop-types';
+import React from "react";
+import styles from "./Column.scss";
+import PropTypes from "prop-types";
+import Card from "../Card/Card.js";
+import Icon from "../Icon/Icon";
+import { settings } from "../../data/dataStore";
+import Creator from "../Creator/Creator";
 
 class Column extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+    cards: PropTypes.array,
+    icon: PropTypes.string,
+    addCards: PropTypes.func,
+  };
+
   render() {
+    console.log("propsy", this.props);
     return (
       <section className={styles.component}>
-          <h3 className={styles.title}>{this.props.title}</h3>
+        <h3 className={styles.title}>
+          <span className={styles.icon}>
+            <Icon name={this.props.icon} />
+          </span>
+          {this.props.title}
+        </h3>
+        <div className={styles.cards}>
+          {this.props.cards.map((card) => (
+            <Card key={card.id} {...card} />
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator
+            text={settings.cardCreatorText}
+            action={(title) => this.addCards(title)}
+          />
+        </div>
       </section>
-    )
+    );
   }
 }
 
